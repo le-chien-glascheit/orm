35c023062a660c0d0d9683c75a367f09502a779a
Vladislav <166797675+le-chien-glascheit@users.noreply.github.com>
2024-07-23 14:46:44 +0300

    Реализовано заполнение Task
    так же добавлен запрос на кол-во товаров удовлетворяющих условию в Task: 
    select task.id, sum(productquantity.quantity) as products_count
    from task
    JOIN productquantity on productquantity.task_id = task.id
    GROUP by task.id

--- orm.py
+++ orm.py
@@ -3,8 +3,10 @@ from collections.abc import Callable
 import string
 from datetime import datetime
 import random
+from typing import Optional
 from uuid import uuid4
 
+from sqlalchemy.sql import func
 from sqlalchemy.orm import declared_attr, DeclarativeBase, Mapped, \
     mapped_column, relationship, Session, joinedload
 from sqlalchemy import ForeignKey, create_engine, UUID, Uuid, select
@@ -25,9 +27,8 @@ class NameMixin:
 class Product(NameMixin, Base):
     barcode: Mapped[int]
 
-    tasks: Mapped[list['Task']] = relationship(
-        secondary='producttakmtm',
-        back_populates='products',
+    tasks: Mapped[list['ProductQuantity']] = relationship(
+        back_populates='product'
     )
 
 
@@ -51,10 +52,8 @@ class Device(NameMixin, Base):
 
 class Task(Base):
     date: Mapped[datetime] = mapped_column(default=datetime.now)
-
-    products: Mapped[list[Product]] = relationship(
-        secondary='producttakmtm',
-        back_populates='tasks'
+    products: Mapped[list['ProductQuantity']] = relationship(
+        back_populates='task'
     )
 
     devices: Mapped[list['Device']] = relationship(
@@ -68,10 +67,16 @@ class Task(Base):
     )
 
 
-class ProductTakMtm(Base):
+class ProductQuantity(Base):
     product_id: Mapped[int] = mapped_column(ForeignKey(Product.id))
     task_id: Mapped[int] = mapped_column(ForeignKey(Task.id))
     quantity: Mapped[int | None] = mapped_column(default=None, nullable=True)
+    task: Mapped['Task'] = relationship(
+        back_populates='products'
+    )
+    product: Mapped['Product'] = relationship(
+        back_populates='tasks'
+    )
 
 
 class UserTask(Base):
@@ -88,43 +93,50 @@ def create_tables(engine):
     Base.metadata.create_all(engine)
 
 
-def task1init(session: Session):
-    user1 = User(name='Max', password=574732477)
-    user2 = User(name='Oleg', password=646567475)
-    device = Device(name='SKF_2000', description='Я прибор, а не документ')
-    product = Product(name='Морковь', barcode=237200121)
-    product2 = Product(name='Эстрагон', barcode=237200543)
-    product3 = Product(name='Имбирь', barcode=237200984)
-    task = Task()
-
-    task.users.extend([user1, user2])
-
-    session.add(user1)
-    session.add(user2)
-    session.add(device)
-
-    session.add(product)
-    session.add(product2)
-    session.add(product3)
-
-    session.add(task)
-    session.add(task)
-    session.add(task)
-
-    session.commit()
-
-
 def do_select(session: Session):
     request = session.execute(select(User).options(joinedload(User.tasks)))
     for user in request.unique().scalars().all():
         print(user.name, user.password, [task.id for task in user.tasks])
 
-        # def select_users_who_use_devices(session: Session, Device=None):
-        #     request = session.execute(select(Device).options(joinedload(Device.tasks)))
-        #     for Device in request.unique().scalars().all():
-        #         print(User.name, [task.id for task in Device.tasks])
 
-        data_generator(User, {'name': uuid4, 'password': ...}, 16, session)
+def select_users_who_use_devices(session: Session, Device=None):
+    request = session.execute(select(Device).options(joinedload(Device.tasks)))
+    for Device in request.unique().scalars().all():
+        print(User.name, [task.id for task in Device.tasks])
+
+
+def select_task_id_count_product_id(session: Session):
+    response = session.execute(
+        select(Task.id, func.sum(ProductQuantity.quantity).label("count"))
+        .join(ProductQuantity)
+        .group_by(Task.id)
+    )
+    for row in response:
+        print(row)
+
+
+"""
+select task.id, sum(productquantity.quantity) as products_count
+from task
+JOIN productquantity on productquantity.task_id = task.id
+GROUP by task.id
+"""
+
+
+def product_fabric(session: Session) -> list[ProductQuantity]:
+    products = []
+    product_random = session.execute(
+        select(Product.id).offset(random.randint(1, 24)).limit(
+            50)).scalars().all()
+
+    for _ in range(random.randint(1, 15)):
+        product_quantity = ProductQuantity(
+            quantity=random.randint(0, 99999),
+            product_id=random.choice(product_random),
+        )
+        products.append(product_quantity)
+
+    return products
 
 
 def data_generator(
@@ -139,97 +151,80 @@ def data_generator(
     for _ in range(count):
         data = dict()
         for filed, func in fileds.items():
-            data[filed] = func()
+            data[filed] = func(session)
         session.add(model(**data))
     session.commit()
 
 
-def random_init_users(session: Session, counter: int):
-    for _ in range(counter):
-        name = ''.join(random.choices(
-            string.ascii_lowercase,
-            k=random.randint(3, 9)
-        ))
-        password = random.randint(100000000, 999999999)
-        user = User(name=name, password=password)
-        session.add(user)
-    session.commit()
+def add_task(session: Session):
+    product = Product(name='Морковь', barcode=237200121)
+    product_quantity = ProductQuantity(quantity=15, product=product)
+    task = Task(products=[product_quantity])
 
+    product_gen: Callable[[], list[ProductQuantity]]
+    user_gen: Callable[[], list[User]]
 
-def random_init_products(session: Session, counter: int):
-    for _ in range(counter):
-        name = ''.join(random.choices(
-            string.ascii_lowercase,
-            k=random.randint(7, 24)
-        ))
-        barcode = random.randint(100000000, 999999999)
-        product = Product(name=name, barcode=barcode)
-        session.add(product)
-    session.commit()
-
-
-def random_init_device(session: Session, counter: int):
-    for _ in range(counter):
-        name = 'SKF_' + ''.join(random.choices(
-            # string.ascii_lowercase,
-            string.hexdigits,
-            k=random.randint(3, 12)
-        ))
-        description = ''.join(random.choices(
-            string.ascii_lowercase,
-            k=random.randint(24, 170)
-        ))
-        device = Device(name=name, description=description)
-        session.add(device)
+    {'products': product_gen, 'users': user_gen}
+    session.add(task)
+    # session.add(product)
     session.commit()
 
 
 def main():
-    engine = create_engine('sqlite:///db.sqlite3', echo=True)
+    engine = create_engine('sqlite:///db.sqlite3', echo=False)
     create_tables(engine)
     with Session(engine) as session:
-        # task1init(session)
-        data_generator(
-            model=User,
-            fileds={
-                'name': lambda: ''.join(random.choices(
-                    string.ascii_lowercase,
-                    k=random.randint(7, 24)
-                )),
-                'password': lambda: random.randint(100000000, 999999999),
-            },
-            count=15,
-            session=session
+        # add_task(session)
+        # data_generator(
+        #     model=User,
+        #     fileds={
+        #         'name': lambda*_: ''.join(random.choices(
+        #             string.ascii_lowercase,
+        #             k=random.randint(7, 24)
+        #         )),
+        #         'password': lambda*_: random.randint(100000000, 999999999),
+        #     },
+        #     count=150,
+        #     session=session
+        # )
+        # data_generator(
+        #     model=Product,
+        #     fileds={
+        #         'name': lambda*_: ''.join(random.choices(
+        #             string.ascii_lowercase,
+        #             k=random.randint(7, 24))),
+        #         'barcode': lambda*_: random.randint(100000000, 999999999)
+        #     },
+        #     count=1300,
+        #     session=session
+        # )
+        # data_generator(
+        #     model=Device,
+        #     fileds={
+        #         'name': lambda*_: 'SKF_' + ''.join(random.choices(
+        #             string.hexdigits,
+        #             k=random.randint(3, 12))),
+        #         'description': lambda*_: ''.join(random.choices(
+        #             string.ascii_letters + ' ',
+        #             k=random.randint(24, 170)))
+        #     },
+        #     count=50,
+        #     session=session
+        # )
 
-        )
-        data_generator(
-            model=Product,
-            fileds={
-                'name': lambda: ''.join(random.choices(
-                    string.ascii_lowercase,
-                    k=random.randint(7, 24))),
-                'barcode': lambda: random.randint(100000000, 999999999)
-            },
-            count=1300,
-            session=session
-        )
+        # data_generator(
+        #     model=Task,
+        #     fileds={
+        #         'products': product_fabric,
+        #
+        #     },
+        #     count=70,
+        #     session=session,
+        # )
 
-        data_generator(
-            model=Device,
-            fileds={
-                'name': lambda: 'SKF_' + ''.join(random.choices(
-                    string.hexdigits,
-                    k=random.randint(3, 12))),
-                'description': lambda: ''.join(random.choices(
-                    string.ascii_letters + ' ',
-                    k=random.randint(24, 170)))
-            },
-            count=50,
-            session=session
-        )
-        # random_init_users(session, 20000)
-        # random_init_products(session, 35768)
-        # random_init_device(session, 36489)
+        select_task_id_count_product_id(session)
+        # add_task(session)
+
         # do_select(session)
         # select_users_who_use_devices(session)
 
